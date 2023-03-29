22058fcc1944d46f1d38f56f475785c52d77c124
Danivsh <rroninway@gmail.com>
2023-03-29 21:50:12 +0400

    added file 6.js

---
+++ codewars/6.js
@@ -0,0 +1,15 @@
+const min = list => {
+  let m = list[0];
+  for(let i = 1; i < list.length; i++) {
+    if(list[i] < m) m = list[i];
+  }
+  return m;
+}
+
+const max = list => {
+  let m = list[0];
+  for(let i = 1; i < list.length; i++) {
+    if(list[i] > m) m = list[i];
+  }
+  return m;
+}
