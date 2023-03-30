af2e2786d6bcd4aefea97cb6d26f651c6eb8b394
Danivsh <rroninway@gmail.com>
2023-03-30 15:18:56 +0400

    add 11.js

---
+++ codewars/11.js
@@ -0,0 +1,17 @@
+function basicOp(operation, value1, value2) {
+  let res;
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+      break;
+    case "-":
+      return value1 - value2;
+      break;
+    case "*":
+      return value1 * value2;
+      break;
+    case "/":
+      return value1 / value2;
+      break;
+  }
+}
